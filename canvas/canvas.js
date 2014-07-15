9980a0c8fdf3686832a57215072317d5fef110b9
hudamin <hudamin@baidu.com>
2014-07-15 19:22:50 +0800

    master: canvas: add lib/Canvas.js

--- canvas/canvas.js
+++ canvas/canvas.js
@@ -1,160 +1,148 @@
 $(function(){
 
-var canvas = $('canvas')[0],
-    context = null;;
+var canvas = new Canvas($('<canvas></canvas>').appendTo('body'));
 
-if(canvas.getContext){
-    context = canvas.getContext('2d');
+
+function drawText(){
+
+    canvas
+        /*
+        .width($(window).width() * 0.9)
+        .height($(window).height() * 0.9)
+        */
+        .width(640)
+        .height(1000)
+
+        .font('italic bold 28px Arial')
+
+        .save()
+        .moveTo(0, 100)
+        .lineTo(100, 100)
+        .strokeStyle('red')
+        .stroke()
+
+        .font('40px monospace')
+        .fillStyle('blue')
+        .textBaseline('top')
+        .fillText('TOP', 50, 100)
+        .textBaseline('bottom')
+        .fillText('HELLO, WORLD', 50, 100)
+        .restore()
+        .fillText('HELLO, Canvas', 50, 120)
+        ;
+
+    canvas
+        .save()
+        .globalAlpha(0.9)
+        .fillStyle('#9033eb')
+        .textBaseline('top')
+        .fillText('hello, canvas', 10, 10)
+        .restore()
+        ;
+
+
 }
 
-if(!context){
-    return;
+
+function drawLine(){
+
+    var randY = [];
+    for(var i=0; i<canvas.getWidth(); i+=60){
+        randY.push(320.5 + Math.floor(Math.random() * 200) );
+    }
+
+
+    // background
+    canvas.save()
+        .globalAlpha(1)
+        .lineWidth(2)
+        .fillStyle('#7ea4b8')
+        .fillRect(0, 300, canvas.getWidth(), 220)
+        .restore()
+        ;
+
+
+
+    // grids
+    canvas.save()
+        .globalAlpha(0.2)
+        .lineWidth(1)
+        .strokeStyle('#f8f8f8')
+        ;
+    for(var i=0, j=0; i<canvas.getWidth(); i+=60, j++){
+        canvas
+            .moveTo(i+0.5, randY[j] + 8)
+            .lineTo(i+0.5, 300.5 + 220)
+            ;
+    }
+    canvas
+        .stroke()
+        .restore()
+        ;
+
+
+
+
+
+    // lines
+    canvas.save()
+        .globalAlpha(1)
+        .lineWidth(2)
+        .strokeStyle('#ffffff')
+        .beginPath()
+        ;
+
+    for(var i=0, j=0; i<canvas.getWidth(); i+=60, j++){
+        if(0 == i){
+            canvas
+                .moveTo(i+0.5, randY[j])
+                ;
+        }
+        else{
+            canvas
+                .lineTo(i+0.5, randY[j])
+                ;
+        }
+    }
+    canvas
+        .stroke()
+        .restore()
+        ;
+
+
+
+    // intersections
+    canvas.save()
+        .globalAlpha(1)
+        .lineWidth(2)
+        .strokeStyle('#ffffff')
+        .fillStyle('#7ea4b8')
+        ;
+
+    for(var i=0, j=0; i<canvas.getWidth(); i+=60, j++){
+        canvas
+            .beginPath()
+            .moveTo(i+0.5+4, randY[j])
+            .arc(i+0.5, randY[j], 8, 0, 2 * Math.PI, false)
+            .closePath()
+            .stroke()
+
+            .beginPath()
+            .moveTo(i+0.5+3, randY[j])
+            .arc(i+0.5, randY[j], 7, 0, 2 * Math.PI, false)
+            .closePath()
+            .fill()
+            ;
+    }
+    canvas
+        .restore()
+        ;
 }
 
-function draw(){
-
-    // 圆弧
-    var param = {
-        ox: 50
-        , oy: 20
-        , radius: 30
-        , startAngle: 0
-        , endAngle: Math.PI * 1 
-        , anticlockwise: false
-    };
-
-    context.beginPath();
-    context.moveTo(0, 10);
-    context.lineTo(100, 10);
-    context.arc(
-        param.ox
-        , param.oy
-        , param.radius
-        , param.startAngle
-        , param.endAngle
-        , param.anticlockwise
-    );
-    context.closePath();
-    context.stroke();
-
-
-
-
-
-    // 相切弧
-    var param = {
-        lastx: 260
-        , lasty: 10
-        , cpx: 300 
-        , cpy: 10 
-        , endx: 300 
-        , endy: 50 
-        , radius: 40
-    };
-    context.beginPath();
-    context.moveTo(150, 10);
-    context.lineTo(param.lastx, param.lasty);
-    context.arcTo(
-        param.cpx
-        , param.cpy
-        , param.endx
-        , param.endy
-        , param.radius
-    );
-    context.lineTo(param.endx, param.endy + 50);
-    context.strokeStyle = '#00f';
-    context.stroke();
-
-
-
-
-    // 贝塞尔曲线
-    var param = {
-        startx: 0
-        , starty: 300
-        , cpx1: 100 
-        , cpy1: 200 
-        , cpx2: 220 
-        , cpy2: 200 
-        , endx: 320 
-        , endy: 300 
-    };
-
-    context.save();
-    context.rotate(Math.PI * 0.08);
-    context.translate(75, -30);
-
-    context.beginPath();
-    context.moveTo(param.startx, param.starty);
-    context.bezierCurveTo(
-        param.cpx1
-        , param.cpy1
-        , param.cpx2
-        , param.cpy2
-        , param.endx
-        , param.endy
-    );
-
-    context.lineTo(param.endx, param.endy - 100);
-    context.lineTo(param.startx, param.starty - 100);
-    context.closePath();
-
-    context.fillStyle = 'rgba(0, 0, 0, 0.3)';
-    context.fill();
-    context.restore();
-
-
-    context.save();
-    context.rotate(Math.PI * 0.08);
-    context.translate(75, -30);
-
-    context.beginPath();
-    context.moveTo(param.startx, param.starty);
-    context.bezierCurveTo(
-        param.cpx1
-        , param.cpy1 + 200
-        , param.cpx2
-        , param.cpy2 + 200
-        , param.endx
-        , param.endy
-    );
-
-    context.lineTo(param.endx, param.endy + 100);
-    context.lineTo(param.startx, param.starty + 100);
-    context.closePath();
-
-    context.fillStyle = 'rgba(0, 0, 0, 0.3)';
-    context.fill();
-    context.restore();
-
-
-
-    // 直接绘制
-    context.fillRect(100, 30, 50, 60);
-
-
-
-
-
-    // 清除指定区域
-    var param = {
-        left: 0 
-        , top: 40 
-        , width: 320 
-        , height: 15 
-    };
-    context.clearRect(
-        param.left
-        , param.top
-        , param.width
-        , param.height
-    );
-
-
-    console.log(context);
-}
-
-draw();
+setTimeout(function(){
+    drawText();
+    drawLine();
+}, 300);
 
 
 });
