dc1980b2ed865731697888abb363de3731c45278
hudamin <hudamin@baidu.com>
2014-07-16 15:23:16 +0800

    master: add lib/Utils.js, and update canvas.js

--- canvas/canvas.js
+++ canvas/canvas.js
@@ -61,7 +61,7 @@ function drawLine(){
 
     // background
     canvas.save()
-        .globalAlpha(1)
+        .globalAlpha(0.5)
         .lineWidth(2)
         .fillStyle('#7ea4b8')
         .fillRect(0, 0, canvas.getWidth(), 320)
@@ -123,7 +123,6 @@ function drawLine(){
         .globalAlpha(1)
         .lineWidth(2)
         .strokeStyle('#ffffff')
-        .fillStyle('#7ea4b8')
         ;
 
     for(var i=10, j=0; i<canvas.getWidth(); i+=60, j++){
@@ -134,11 +133,15 @@ function drawLine(){
             .closePath()
             .stroke()
 
+            .save()
             .beginPath()
+            .globalAlpha(0.5)
+            .fillStyle('#7ea4b8')
             .moveTo(i+0.5+3, randY[j])
             .arc(i+0.5, randY[j], 7, 0, 2 * Math.PI, false)
             .closePath()
             .fill()
+            .restore()
             ;
     }
     canvas
@@ -179,9 +182,173 @@ function drawLine(){
         
 }
 
+
+
+function drawLine2(){
+
+    var canvas = new Canvas($('<canvas id="c_lines2"></canvas>').appendTo('body'));
+
+    var X = [], randY = [];
+
+    canvas
+        .width(1280)
+        .height(400);
+
+    for(var i=0; i<canvas.getWidth(); i+=60){
+        X.push(10 + i + 0.5); 
+        randY.push(20.5 + Math.floor(Math.random() * 300) );
+    }
+
+
+    // background
+    canvas.save()
+        .globalAlpha(0.5)
+        .lineWidth(2)
+        .fillStyle('#7ea4b8')
+        .fillRect(0, 0, canvas.getWidth(), 320)
+        .restore()
+        ;
+
+
+
+    // grids
+    canvas.save()
+        .globalAlpha(0.4)
+        .lineWidth(1)
+        .strokeStyle('#f8f8f8')
+        ;
+    for(var i=0; i<X.length; i++){
+        canvas
+            .moveTo(X[i], randY[i] + 8)
+            .lineTo(X[i], 20.5 + 320)
+            ;
+    }
+    canvas
+        .stroke()
+        .restore()
+        ;
+
+
+
+    // intersections
+    canvas.save()
+        .globalAlpha(1)
+        .lineWidth(2)
+        .strokeStyle('#ffffff')
+        .fillStyle('#7ea4b8')
+        ;
+
+    for(var i=0; i<X.length; i++){
+        canvas
+            .beginPath()
+            .moveTo(X[i]+8, randY[i])
+            .arc(X[i], randY[i], 8, 0, 2 * Math.PI, false)
+            .closePath()
+            .stroke()
+            ;
+    }
+    canvas
+        .restore()
+        ;
+
+
+
+
+    // lines
+    canvas.save()
+        .globalAlpha(1)
+        .lineWidth(2)
+        .strokeStyle('#ffffff')
+        ;
+
+    var fromCircle, toCircle, line, intersects,
+        fromPoint = {x:0, y:0}, 
+        toPoint = {x:0, y:0};
+
+    for(var i=1; i<X.length; i++){
+        fromCircle = {a: X[i-1], c: randY[i-1], r: 8}; 
+        toCircle = {a: X[i], c: randY[i], r: 8}; 
+        line = get_line_with_2points(
+            {x: X[i-1], y: randY[i-1]}
+            , {x: X[i], y: randY[i]}
+        );
+
+        intersects = get_intersect_between_line_and_circle(line, fromCircle); 
+
+        if(intersects){
+            fromPoint = intersects.length == 2
+                ? intersects[1] : intersects[0];
+        }
+
+        intersects = get_intersect_between_line_and_circle(line, toCircle); 
+
+        if(intersects){
+            toPoint = intersects[0];
+        }
+
+        /*
+        console.log([
+                '[' + X[i-1] + ',' + randY[i-1] + ']'        
+                , '[' + fromPoint.x + ',' + fromPoint.y + ']'        
+                , '[' + toPoint.x + ',' + toPoint.y + ']'        
+                , '[' + X[i] + ',' + randY[i] + ']'        
+            ].join(' ')
+        );
+        */
+
+        canvas.beginPath()
+            .moveTo(fromPoint.x, fromPoint.y)
+            .lineTo(toPoint.x, toPoint.y)
+            .stroke()
+            ;
+
+    }
+
+    canvas.restore()
+        ;
+
+
+
+    var initTouchX, initPos, isBusy = false;
+    canvas.on('touchstart', function(e){
+            console.log('touchstart');
+            initTouchX 
+                = e.targetTouches[0].clientX;
+            initPos = $(e.target).offset();
+        })
+        .on('touchmove', function(e){
+            var t = e.targetTouches[0],
+                offsetX;
+
+            e.preventDefault();
+
+            offsetX = t.clientX - initTouchX;
+            console.log(offsetX);
+
+            if(isBusy){
+                return;
+            }
+            isBusy = true;
+            setTimeout(function(){ isBusy = false; }, 20);
+
+            $(canvas.canvas).css({
+                '-webkit-transform': 'translate(' + ( initPos.left - 0 + offsetX ) + 'px, 0px)',
+            });
+        })
+        .on('touchend', function(e){
+            console.log('touchend');
+        });
+
+        
+}
+
+
+
+
 setTimeout(function(){
     drawText();
     drawLine();
+    drawLine2();
 }, 300);
 
 
--- canvas/lib/Utils.js
+++ canvas/lib/Utils.js
@@ -1,1 +1,95 @@
-function 
+function get_line_with_2points(p1, p2){
+
+    var line = {};    
+
+    if(p1.x - p2.x != 0){
+        line.k = (p2.y - p1.y) / (p2.x - p1.x);
+        line.b = p1.y - line.k * p1.x;
+    }
+    else{
+        return null;
+    }
+
+    return line;
+
+}
+
+function get_line_with_point_and_gradient(p1, k){
+    var line = {}; 
+    
+    line.k = k;
+    line.b = p1.y - line.k * p1.x;
+} 
+
+function get_distance_between_2points(p1, p2){
+    return Math.sqrt(
+            Math.pow(p2.y - p1.y, 2)
+            + Math.pow(p2.x - p1.x, 2)
+        );
+}
+
+/**
+ * |- line: y = kx + b
+ * |- circle: (x - a)^2 + (y - c)^2 = r^2
+ * 
+ * (1 + k^2)x^2 + 2(bk - ck -a)x + a^2 + b^2 + c^2 - 2bc - r^2 = 0
+ * 
+ * A = 1 + k^2
+ * B = 2(bk - ck -a)
+ * C = a^2 + b^2 + c^2 - 2bc - r^2
+ * 
+ * if B^2 - 4AC > 0
+ * x1 = ( -B + sqrt(B^2 - 4AC) ) / 2A 
+ * x2 = ( -B - sqrt(B^2 - 4AC) ) / 2A 
+ * y1 = kx1 + b
+ * y2 = kx2 + b
+ * 
+ * if B^2 - 4AC = 0
+ * x1 = x2 = -B / 2A
+ * y1 = y2 = kx1 + b
+ * 
+ * if B^2 - 4AC < 0
+ * no result
+ */
+function get_intersect_between_line_and_circle(line, circle){
+    var points = [],
+        k = line.k,
+        b = line.b,
+        a = circle.a,
+        c = circle.c,
+        r = circle.r, 
+
+        A = 1 + k * k,
+        B = 2 * ( b * k - c * k - a ),
+        C = a * a + b * b + c * c - 2 * b * c - r * r,
+
+        delta = B * B - 4 * A * C,
+        x1, y1, x2, y2,
+
+        sqrt = Math.sqrt,
+        pow = Math.pow
+        ;
+         
+    if( delta > 0 ){
+        x1 = ( -B - sqrt( delta ) ) / ( 2 * A ); 
+        y1 = k * x1 + b;
+
+        x2 = ( -B + sqrt( delta ) ) / ( 2 * A ); 
+        y2 = k * x2 + b;
+
+        points.push({x: x1, y: y1});
+        points.push({x: x2, y: y2});
+    }
+    else if( delta == 0 ){
+        x1 = -B / ( 2 * A );
+        y1 = k * x1 + b;
+        points.push({x: x1, y: y1});
+    }
+    else{
+        return null;
+    }
+
+    return points;
+}
+
+
